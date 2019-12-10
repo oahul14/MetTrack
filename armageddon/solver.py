e40ebc8cd42aeb044226af3aabc7550702b9fcee
acse-hl1319 <56033310+acse-hl1319@users.noreply.github.com>
2019-12-10 09:50:35 +0000

    Update solver.py
    Including changes in outcome_analysis function and variables in the previous solver.

--- armageddon/solver.py
+++ armageddon/solver.py
@@ -14,49 +14,35 @@ class Planet():
                  g=9.81, H=8000., rho0=1.2):
         """
         Set up the initial parameters and constants for the target planet
-
         Parameters
         ----------
-
         atmos_func : string, optional
             Function which computes atmospheric density, rho, at altitude, z.
             Default is the exponential function ``rho = rho0 exp(-z/H)``.
             Options are ``exponential``, ``tabular``, ``constant`` and ``mars``
-
         atmos_filename : string, optional
             If ``atmos_func`` = ``'tabular'``, then set the filename of the table
             to be read in here.
-
         Cd : float, optional
             The drag coefficient
-
         Ch : float, optional
             The heat transfer coefficient
-
         Q : float, optional
             The heat of ablation (J/kg)
-
         Cl : float, optional
             Lift coefficient
-
         alpha : float, optional
             Dispersion coefficient
-
         Rp : float, optional
             Planet radius (m)
-
         rho0 : float, optional
             Air density at zero altitude (kg/m^3)
-
         g : float, optional
             Surface gravity (m/s^2)
-
         H : float, optional
             Atmospheric scale height (m)
-
         Returns
         -------
-
         None
         """
 
@@ -82,6 +68,7 @@ class Planet():
             self.rhoa = lambda x: rho0
         else:
             raise NotImplementedError
+
     def f(self, t, state):
         """ Coupled ODEs when ram pressure is 
         below strength (no changes in radius)
@@ -99,7 +86,7 @@ class Planet():
         f[5] = 0
         return f
 
-    def f2(self, t, state):
+    def f2(self, t, state, density):
         """ Coupled ODEs when ram pressure is 
         above strength (changes in radius and in area)
         """
@@ -108,15 +95,15 @@ class Planet():
         v, m, theta, z, x, r = state  
         atmo_den = 1.2*np.exp(-z/8000)
         A = np.pi*r**2
-        f2[0] = -(Cd*atmo_den*A*v**2)/(2*m) + g*np.sin(theta)
-        f2[1] = -(Ch*atmo_den*A*v**3)/(2*Q)
-        f2[2] = (g*np.cos(theta))/v - (Cl*atmo_den*A*v)/(2*m) - (v*np.cos(theta))/(Rp+z)
+        f2[0] = -(self.Cd*atmo_den*A*v**2)/(2*m) + self.g*np.sin(theta)
+        f2[1] = -(self.Ch*atmo_den*A*v**3)/(2*self.Q)
+        f2[2] = (self.g*np.cos(theta))/v - (self.Cl*atmo_den*A*v)/(2*m) - (v*np.cos(theta))/(self.Rp+z)
         f2[3] = -v*np.sin(theta)
-        f2[4] = v*np.cos(theta)/(1+z/Rp)
-        f2[5] = np.sqrt(7/2*alpha*atmo_den/met_den)*v
+        f2[4] = v*np.cos(theta)/(1+z/self.Rp)
+        f2[5] = np.sqrt(7/2*self.alpha*atmo_den/density)*v
         return f2
 
-    def RK4(self, u0, t0, t_max, dt, Y):
+    def RK4(self, u0, t0, t_max, dt, Y, density):
         u = np.array(u0)
         t = np.array(t0)
         u_all = [u0]
@@ -124,10 +111,10 @@ class Planet():
         while t < t_max:
             atmo_den = 1.2*np.exp(u[3]/8000)
             if atmo_den*u[0]**2 > Y:
-                k1 = dt*self.f2(t, u)
-                k2 = dt*self.f2(t + 0.5*dt, u + 0.5*k1)
-                k3 = dt*self.f2(t + 0.5*dt, u + 0.5*k2)
-                k4 = dt*self.f2(t + dt, u + k3) 
+                k1 = dt*self.f2(t, u, density)
+                k2 = dt*self.f2(t + 0.5*dt, u + 0.5*k1, density)
+                k3 = dt*self.f2(t + 0.5*dt, u + 0.5*k2, density)
+                k4 = dt*self.f2(t + dt, u + k3, density) 
             else: #if below threshold => 
                 k1 = dt*self.f(t, u)
                 k2 = dt*self.f(t + 0.5*dt, u + 0.5*k1)
@@ -138,76 +125,62 @@ class Planet():
             t = t + dt
             t_all.append(t)              
         return np.array(u_all), np.array(t_all)
+
     def impact(self, radius, velocity, density, strength, angle,
                init_altitude=100e3, dt=0.05, radians=False):
         """
         Solve the system of differential equations for a given impact event.
         Also calculates the kinetic energy lost per unit altitude and
         analyses the result to determine the outcome of the impact.
-
         Parameters
         ----------
-
         radius : float
             The radius of the asteroid in meters
-
         velocity : float
             The entery speed of the asteroid in meters/second
-
         density : float
             The density of the asteroid in kg/m^3
-
         strength : float
             The strength of the asteroid (i.e., the ram pressure above which
             fragmentation and spreading occurs) in N/m^2 (Pa)
-
         angle : float
             The initial trajectory angle of the asteroid to the horizontal
             By default, input is in degrees. If 'radians' is set to True, the
             input should be in radians
-
         init_altitude : float, optional
             Initial altitude in m
-
         dt : float, optional
             The output timestep, in s
-
         radians : logical, optional
             Whether angles should be given in degrees or radians. Default=False
             Angles returned in the DataFrame will have the same units as the
             input
-
         Returns
         -------
-
         Result : DataFrame
             A pandas DataFrame containing the solution to the system.
             Includes the following columns:
             ``velocity``, ``mass``, ``angle``, ``altitude``,
             ``distance``, ``radius``, ``time``, ``dedz``
-
         outcome : Dict
             dictionary with details of airburst and/or cratering event.
             For an airburst, this will contain the following keys:
             ``burst_peak_dedz``, ``burst_altitude``, ``burst_total_ke_lost``.
-
             For a cratering event, this will contain the following keys:
             ``impact_time``, ``impact_mass``, ``impact_speed``.
-
             All events should also contain an entry with the key ``outcome``,
             which should contain one of the following strings:
             ``Airburst``, ``Cratering`` or ``Airburst and cratering``
         """
         m=density*4/3*np.pi*radius**3
         state0 = np.array([velocity, m, angle, init_altitude,0, radius])
-        X = self.RK4(state0,0, 20, 0.01, strength)
+        X = self.RK4(state0,0, 20, 0.01, strength, density)
         #dedz= np.array(1/2*X[0][:, 1]*X[0][:, 0]**2)
         #dedz = abs(np.diff(dedz))
         result = np.zeros((len(X[0][:, 0])-1, 7))
         result[:, 0:-1] = X[0][:-1, :]
         result[:, -1] = X[1][:-1]
-        result = pd.DataFrame(result, columns=["velocity", "mass", "angle", "altitude",
-    "distance", "radius", "time"])   
+        result = pd.DataFrame(result, columns=["velocity", "mass", "angle", "altitude", "distance", "radius", "time"])   
         result = self.calculate_energy(result)
         result = result.fillna(0)
         plt.figure()
@@ -220,39 +193,29 @@ class Planet():
             init_altitude=100e3, dt=0.05, radians=False):
         """
         Solve the system of differential equations for a given impact scenario
-
         Parameters
         ----------
-
         radius : float
             The radius of the asteroid in meters
-
         velocity : float
             The entery speed of the asteroid in meters/second
-
         density : float
             The density of the asteroid in kg/m^3
-
         strength : float
             The strength of the asteroid (i.e., the ram pressure above which
             fragmentation and spreading occurs) in N/m^2 (Pa)
-
         angle : float
             The initial trajectory angle of the asteroid to the horizontal
             By default, input is in degrees. If 'radians' is set to True, the
             input should be in radians
-
         init_altitude : float, optional
             Initial altitude in m
-
         dt : float, optional
             The output timestep, in s
-
         radians : logical, optional
             Whether angles should be given in degrees or radians. Default=False
             Angles returned in the DataFrame will have the same units as the
             input
-
         Returns
         -------
         Result : DataFrame
@@ -264,7 +227,7 @@ class Planet():
 
         m=3000*4/3*np.pi*10**3
         state0 = np.array([velocity, m, angle, init_altitude,0, radius])
-        X = self.RK4(state0,0, 20, 0.01, strength)
+        X = self.RK4(state0,0, 20, 0.01, strength, density)
         return pd.DataFrame({'velocity': X[0][-1, 0],
                              'mass': X[0][-1, 1],
                              'angle': X[0][-1, 2],
@@ -277,17 +240,13 @@ class Planet():
         """
         Function to calculate the kinetic energy lost per unit altitude in
         kilotons TNT per km, for a given solution.
-
         Parameters
         ----------
-
         result : DataFrame
             A pandas DataFrame with columns for the velocity, mass, angle,
             altitude, horizontal distance and radius as a function of time
-
         Returns
         -------
-
         Result : DataFrame
             Returns the DataFrame with additional column ``dedz`` which is the
             kinetic energy lost per unit altitude
@@ -328,78 +287,6 @@ class Planet():
             which should contain one of the following strings:
             ``Airburst``, ``Cratering`` or ``Airburst and cratering``
         """
-        def airburst(result, row_maxdedz):
-            """
-            define a function to calculate the outcome when altitude > 5
-            """
-            # calculate the total energy loss till peak energy loss rate
-            # altitude data before peak point
-            zi = np.array(result.loc[0:row_maxdedz.index[0], 'altitude'])
-            # energy loss rate data before peak point
-            dedzi = np.array(result.loc[0:row_maxdedz.index[0], 'dedz'])
-            # number of trapezoid
-            number_intervals = len(zi)-1
-            I_T = 0.0
-            for i in range(number_intervals):
-                # add in the area of the interval shape to our running total using trapezoid formula
-                I_T += np.abs(((dedzi[i+1] + dedzi[i])/2)*(zi[i+1] - zi[i]))
-
-            outcome = {
-                "outcome": "Airburst",
-                "burst_peak_dedz": row_maxdedz.dedz.iloc[0],
-                "burst_altitude": row_maxdedz.altitude.iloc[0],
-                "burst_total_ke_lost" : I_T
-            }
-            return outcome
-
-        def craburst(result, row_maxdedz):
-            """
-            define a function to calculate the outcome when 0 <=altitude <= 5
-            """
-            # find the first row where altitude < 0 
-            row_alt = result.loc[result.altitude < 0]
-            # use the row before it to get data for cratering event
-            row_alt0 = result.loc[result.index == row_alt.index[0]-1]
-            
-            # calculate the total energy loss till peak energy loss rate
-            # altitude data before peak point
-            zi = np.array(result.loc[0:row_maxdedz.index[0], 'altitude'])
-            # energy loss rate data before peak point
-            dedzi = np.array(result.loc[0:row_maxdedz.index[0], 'dedz'])
-            # number of trapezoid
-            number_intervals = len(zi)-1
-            I_T = 0.0
-            for i in range(number_intervals):
-                # add in the area of the interval shape to our running total using trapezoid formula
-                I_T += np.abs(((dedzi[i+1] + dedzi[i])/2)*(zi[i+1] - zi[i]))
-                
-            outcome = {
-                "outcome": "Airburst and cratering",
-                "burst_peak_dedz": row_maxdedz.dedz.iloc[0],
-                "burst_altitude": row_maxdedz.dedz.iloc[0],
-                "burst_total_ke_lost" : I_T,
-                "impact_time" : row_alt0.time.iloc[0],
-                "impact_mass" :row_alt0.mass.iloc[0],
-                "impact_speed" :row_alt0.velocity.iloc[0]
-            }
-            return outcome
-
-        def cratering(result):
-            """
-            define a function to calculate the outcome when altitude < 0
-            """
-            # find the first row where altitude < 0 
-            row_alt = result.loc[result.altitude < 0]
-            # use the row before it to get data for cratering event
-            row_alt0 = result.loc[result.index == row_alt.index[0]-1]
-            
-            outcome = {
-                "outcome": "Cratering",
-                "impact_time" : row_alt0.time.iloc[0],
-                "impact_mass" :row_alt0.mass.iloc[0],
-                "impact_speed" :row_alt0.velocity.iloc[0]
-            }
-            return outcome
         # define outcome as a dictionary
         outcome = {}
         # find the maxium dedz and its corresponding burst altitude
@@ -414,6 +301,71 @@ class Planet():
             outcome = craburst(result, row_maxdedz)
         elif burst_alt < 0:
             outcome = cratering(result)
+        return outcome
+
+    def airburst(result, row_maxdedz):
+        """
+        define a function to calculate the outcome when altitude > 5
+        """
+        # calculate the total energy loss till peak energy loss rate
+        # m,v at airburst point and initial condition
+        m_burst = result.loc[row_maxdedz.index[0], 'mass']
+        v_burst = result.loc[row_maxdedz.index[0], 'velocity']
+        m0 = result.loc[0, 'mass']
+        v0 = result.loc[0, 'velocity']
+        total_loss = 0.5*(m_burst*v_burst**2-m0*v0**2)
+
+        outcome = {
+            "outcome": "Airburst",
+            "burst_peak_dedz": row_maxdedz.dedz.iloc[0],
+            "burst_altitude": row_maxdedz.altitude.iloc[0],
+            "burst_total_ke_lost" : total_loss
+        }
+        return outcome
+
+    def craburst(result, row_maxdedz):
+        """
+        define a function to calculate the outcome when 0 <=altitude <= 5
+        """
+        # find the first row where altitude < 0 
+        row_alt = result.loc[result.altitude < 0]
+        # use the row before it to get data for cratering event
+        row_alt0 = result.loc[result.index == row_alt.index[0]-1]
+        
+        # calculate the total energy loss till peak energy loss rate
+        # m,v at airburst point and initial condition
+        m_burst = result.loc[row_maxdedz.index[0], 'mass']
+        v_burst = result.loc[row_maxdedz.index[0], 'velocity']
+        m0 = result.loc[0, 'mass']
+        v0 = result.loc[0, 'velocity']
+        total_loss = 0.5*(m_burst*v_burst**2-m0*v0**2)
+        
+        outcome = {
+            "outcome": "Airburst and cratering",
+            "burst_peak_dedz": row_maxdedz.dedz.iloc[0],
+            "burst_altitude": row_maxdedz.dedz.iloc[0],
+            "burst_total_ke_lost" : total_loss,
+            "impact_time" : row_alt0.time.iloc[0],
+            "impact_mass" :row_alt0.mass.iloc[0],
+            "impact_speed" :row_alt0.velocity.iloc[0]
+        }
+        return outcome
+
+    def cratering(result):
+        """
+        define a function to calculate the outcome when altitude < 0
+        """
+        # find the first row where altitude < 0 
+        row_alt = result.loc[result.altitude < 0]
+        # use the row before it to get data for cratering event
+        row_alt0 = result.loc[result.index == row_alt.index[0]-1]
+        
+        outcome = {
+            "outcome": "Cratering",
+            "impact_time" : row_alt0.time.iloc[0],
+            "impact_mass" :row_alt0.mass.iloc[0],
+            "impact_speed" :row_alt0.velocity.iloc[0]
+        }
         return outcome
 
 
