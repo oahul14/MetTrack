4995156160e9a34239a14484975f9e9c8f2c2fd2
Oluwatosin Alagbe <56097497+acse-ota19@users.noreply.github.com>
2019-12-11 10:03:20 +0000

    added sampling method

--- armageddon/ensemble.py
+++ armageddon/ensemble.py
@@ -1,6 +1,48 @@
 import numpy as np
 import pandas as pd
+import scipy.special as ssp
+import scipy.optimize as sop 
 
+class Dist():
+
+    prob_val = 0.1
+
+    def __init__(self, prob_vals):
+        self.prob_vals = prob_vals
+    
+    def velocity_dist(self,v):
+        return ssp.erf(v/(11*np.sqrt(2))) - (v/11)*(np.sqrt(2/np.pi)) * np.exp(-1*(v**2)/(2*(11**2))) - self.prob_val
+
+    def density_dist(self, rho):
+        return 0.5*( 1 + ssp.erf((rho-3000)/(1000*np.sqrt(2))) ) - self.prob_val
+
+    def inverse_radius_distribution(self, rmin, rmax):
+        return self.prob_vals*(rmax-rmin) + rmin 
+
+    def inverse_strength_distribution(self,ymin=1e3,ymax=10e6):
+        return ymin * (10**(self.prob_vals * np.log10(ymax/ymin)))
+
+    def inverse_angle_distribution(self,amin=0,amax=np.pi/2):
+        return np.arccos(np.sqrt(self.prob_vals))
+
+    def inverse_velocity_distribution(self,v_guess=(50-11)/2):
+        v_array = []
+        for prob in self.prob_vals:
+            self.prob_val = prob
+            v_val = sop.newton_krylov(self.velocity_dist,v_guess)
+            v_array.append(v_val)
+            v_np = np.array(v_array)
+        return v_np
+
+    def inverse_density_distribution(self, rho_guess=(3000)):
+        rho_array = []
+        for prob in self.prob_vals:
+            self.prob_val = prob
+            rho_val = sop.diagbroyden(self.density_dist,rho_guess)
+            rho_array.append(rho_val)
+            rho_np = np.array(rho_array)
+        return rho_np
+        
 
 def solve_ensemble(
         planet,
@@ -43,19 +85,44 @@ def solve_ensemble(
         airburst altitude
     """
 
+    #convert to degrees
+    if radians:
+        fiducial_impact['angle'] = fiducial_impact['angle'] * 180/np.pi
+
+    #Number of samples
+    N = 1000
+    prob_distribution = np.random.uniform(0.0,1.0,N)
+
+    distribution = Dist(prob_distribution)
+
+    ensemble_df = pd.DataFrame()
+
     for var in variables:
         # Remove these as you implement each distribution
         if var == 'radius':
-            raise NotImplementedError
+            radius_dist = distribution.inverse_radius_distribution(rmin,rmax)
+            ensemble_df['radius'] = radius_dist
         if var == 'angle':
-            raise NotImplementedError
+            angle_dist = (distribution.inverse_angle_distribution)
+            angle_dist = angle_dist*180/np.pi     #convert to degrees
+            ensemble_df['angle'] = angle_dist
         if var == 'strength':
-            raise NotImplementedError
+            strength_dist = distribution.inverse_strength_distribution()
+            ensemble_df['strength'] = strength_dist
         if var == 'velocity':
-            raise NotImplementedError
+            velocity_dist = distribution.inverse_velocity_distribution()
+            impact_dist = np.sqrt( (11e3)**2 + (velocity_dist*1000)**2 )
+            ensemble_df['velocity'] = impact_dist
         if var == 'density':
-            raise NotImplementedError
+            density_dist = distribution.inverse_density_distribution()
+            ensemble_df['density'] = density_dist
 
+    #check for parameters in fiducial_impact that are not in variables
+    const_vals = np.setdiff1d([*fiducial_impact], variables)
+
+    
     # Implement your ensemble function here
+    ensemble_df = pd.DataFrame
+    planet
 
     return pd.DataFrame(columns=variables+['burst_altitude'], index=range(0))
